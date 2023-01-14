b20a8f98cf4cc08bab135273ea4098b40c0ee224
yuqicxy <yuqicxy@foxmail.com>
2023-01-14 21:23:05 +0800

    add dom routes example

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,31 @@
-import React from "react";
-import Test from './components/TestComponent';
+// import React from "react";
+// import Test from './components/TestComponent';
 
-export default function App(){
-    return <Test>
+// export default function App(){
+//     return <Test>
         
-    </Test>
-}
+//     </Test>
+// }
+
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+const Home = () => <div>Home</div>;
+
+const About = () => <div>About</div>;
+
+const App = () => (
+  <Router>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+  </Router>
+);
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,39 @@ import App from './app';
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material";
 import theme from './theme'
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+  import "./index.css";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root></Root>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children:[
+            {
+                path: "contacts/:contactId",
+                element: <Contact></Contact>
+            }
+        ]
+    },
+    {
+        path: "contacts/1",
+        element: <Contact></Contact>
+    }
+]);
 
 ReactDOM.render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
             <CssBaseline/>
-            <App/>
+            <RouterProvider router={router} />
+            {/* <App></App> */}
         </ThemeProvider>
     </React.StrictMode>,
     document.getElementById('root')
