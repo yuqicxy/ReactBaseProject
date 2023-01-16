95ddf00205199322bec3024ed7567964e6b9c691
yuqicxy <yuqicxy@foxmail.com>
2023-01-16 23:42:22 +0800

    add default index page

--- src/index.jsx
+++ src/index.jsx
@@ -26,6 +26,10 @@ const router = createBrowserRouter([
         action:rootAction,
         children:[
             {
+                index: true, 
+                element: <Index />
+            },
+            {
                 path: "contacts/:contactId",
                 element: <Contact></Contact>,
                 loader:contactLoader,
