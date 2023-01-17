558b3b61ef306e669691b4a60de96c3ec49999c6
yuqicxy <yuqicxy@foxmail.com>
2023-01-17 18:23:37 +0800

    remove abundant comment

--- src/routes/destroy.jsx
+++ src/routes/destroy.jsx
@@ -2,7 +2,6 @@ import { redirect } from "react-router-dom";
 import { deleteContact } from "../contacts"
 
 export async function action({params}){
-    // throw new Error("oh dang!");
-    await deleteContact(params.contactId).then((val)=>{console.log("delete",val,params.contactId);});
+    await deleteContact(params.contactId).then((val)=>{console.log("delete",val,params.contactId);});    
     return redirect("/");
 }
